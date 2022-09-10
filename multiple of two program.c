f09c8b88d5cba45912c7403514e82c5ae85ecd5a
Harshuji <112695789+Harshuji@users.noreply.github.com>
2022-09-10 18:29:30 +0530

    Add files via upload

---
+++ multiple of two program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() { 
+int a = 4;
+float b = 7.9;
+char c = 'N';
+printf("the value of a is %d \n \n \n " ,a);
+printf("the value of b is %f\n\n \n\n" ,b);
+printf("the value of c is %c \n" ,c);
+printf("sum of a * b is %f" ,a * b);
+return 0;
+}
+    
