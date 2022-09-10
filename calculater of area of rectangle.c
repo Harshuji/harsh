588fcd5a4a4d3db9a398d43c1bc0245fbc6eb7cc
Harshuji <112695789+Harshuji@users.noreply.github.com>
2022-09-10 18:29:59 +0530

    Add files via upload

---
+++ calculater of area of rectangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+int length , breadth;
+
+printf("what is the length of rectangle \n");
+scanf("%d", &length);
+
+printf("what is the breadth of rectangle\n");
+scanf("%d", &breadth);
+
+printf("the area of rectangle is  %d" ,length*breadth); 
+return 0;
+}
