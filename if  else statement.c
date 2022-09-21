b01ea8e58da0ec456130bf6e640e6923e3125a4b
Harshuji <112695789+Harshuji@users.noreply.github.com>
2022-09-21 14:57:28 +0530

    Add files via upload

---
+++ if  else statement.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    printf("enter a number \n",a);
+    scanf("%d",&a);
+
+    if(a%2==0) {
+        printf("the number is even");
+    }
+
+    else
+    {
+        printf("the number is odd");
+    }
+}
