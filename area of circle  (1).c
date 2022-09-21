f50e035b1b6cf8bf6edf98198833abc1bb18d166
Harshuji <112695789+Harshuji@users.noreply.github.com>
2022-09-21 14:56:38 +0530

    Add files via upload

---
+++ area of circle  (1).c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+int main()
+{
+float radius;
+float pi = 3.14;
+printf("what is the vale of radius \n");
+scanf("%f",&radius);
+
+printf("the are of circle is %f", radius*radius*pi);
+return 0;
+}
+    
