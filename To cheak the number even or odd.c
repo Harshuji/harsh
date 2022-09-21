5ccca3268921eeef981eff65d5c05c83aa19df0b
Harshuji <112695789+Harshuji@users.noreply.github.com>
2022-09-21 14:56:03 +0530

    Add files via upload

---
+++ To cheak the number even or odd.c
@@ -0,0 +1,20 @@
+// To cheak the number even or odd 
+#include <stdio.h> 
+
+int main() {
+    int a ,b;
+    printf("enter a number\n");
+    scanf("%d" ,&a);
+    
+    if(a%2==0){
+      printf("%d is even \n" ,a);
+    }
+    
+    else {
+         printf("%d is odd\n" ,a);
+        }
+        
+        return 0;
+        }
+    
+
